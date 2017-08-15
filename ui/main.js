4b5555203f1d308cbfe8bd356ee8ed28c1675b9e
haricnite <31013584+haricnite@users.noreply.github.com>
2017-08-15 19:38:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,12 @@ console.log('Loaded!');
 var element=document.getElementById('main');
 element.innerHTML="New Value";
 var image=document.getElementById('i1');
-image.onclick=function() {
-    image.style.marginLeft="100px";
+var marginLeft=0;
+function moveRight() {
+    marginLeft=marginLeft+1;
+    image.style.marginLeft=marginLeft+'px';
     
+}
+img.onclick=function(){
+    var interval=setInterval(moveRight,50);
 };
